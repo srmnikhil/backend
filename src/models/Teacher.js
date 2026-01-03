d95522719a4a31cf421f75c2f25dc4bfa1a92254
Nikhil Sharma <srmnikhilswn@gmail.com>
2026-01-03 21:58:30 +0530

    added Teacher model

---
+++ src/models/Teacher.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const teacherSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+      trim: true,
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Teacher", teacherSchema);
